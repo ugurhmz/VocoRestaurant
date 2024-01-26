aceb7a3d393e617c13c5275e5658d388d4b91f27
ugurhmz <craxx67@gmail.com>
2024-01-26 14:57:00 +0300

    UserModel.js created

---
+++ back-end/models/UserModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    email: { type: String,required: true,unique: true, min: 8},
+    username: { type: String, required: true, min: 3, max: 75 },
+    age: { type: Number, required: true, min: 4 },
+    gender: { type: String },
+    password: { type: String, required: true, min: 6 },
+    firstName: { type: String, required: true, min: 2, max: 100 },
+    lastName: { type: String, required: true, min: 2, max: 100 },
+    activationToken: { type: String },
+    isVerified: { type: Boolean, default: false },
+    profile_img: { type: String, default: "default.png" },
+    addresses: [{ type: String }],
+    resetPasswordToken: { type: String },
+    resetPasswordExpiry: { type: Date },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
