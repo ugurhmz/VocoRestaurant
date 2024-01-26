9e1150c10630447ddbaa6941c652556592596037
ugurhmz <craxx67@gmail.com>
2024-01-26 22:26:33 +0300

    Create order with User Current Address

--- back-end/controller/AddressController.js
+++ back-end/controller/AddressController.js
@@ -6,25 +6,25 @@ export const createAddressController = async (req, res) => {
     const userId = req.user._id
     const { city, district, street, location, isDefault } = req.body
 
+    if (isDefault) {
+      await AddressModel.updateMany({ user: userId }, { $set: { isDefault: false } })
+    }
+
     const newAddress = new AddressModel({
       city,
       district,
       street,
       location,
       isDefault,
+      user: userId,
     })
-  
-    if (isDefault) {
-      await AddressModel.updateMany({ user: userId }, { $set: { isDefault: false } })
-    }
 
-    newAddress.user = userId
     const savedAddress = await newAddress.save()
 
     return res.status(httpStatus.CREATED).json({
       message: 'Address created successfully',
       address: savedAddress,
-    })
+    });
   } catch (error) {
     console.error('createAddressController error:', error)
     return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
--- back-end/models/AddressModel.js
+++ back-end/models/AddressModel.js
@@ -1,16 +1,37 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
 
 const { Schema } = mongoose
 
 const AddressSchema = new Schema(
   {
-    city: { type: String},
-    district: { type: String},
+    city: { type: String },
+    district: { type: String },
     street: { type: String },
     location: { type: String },
     isDefault: { type: Boolean, default: false },
-  }
+    coordinates: {
+      type: {
+        type: String,
+        default: 'Point',
+      },
+      coordinates: {
+        type: [Number],
+        default: [32.8597, 39.9334], // [longitude, latitude]
+      },
+    },
+  },
+  { timestamps: true }
 )
 
-const AddressModel = mongoose.model("Address", AddressSchema)
+AddressSchema.pre('save', async function (next) {
+  if (this.isDefault) {
+    await mongoose.model('Address').updateMany(
+      { _id: { $ne: this._id } },
+      { $set: { isDefault: false } }
+    )
+  }
+  next()
+})
+
+const AddressModel = mongoose.model('Address', AddressSchema)
 export default AddressModel
