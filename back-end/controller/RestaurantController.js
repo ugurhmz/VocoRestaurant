7acb0fe3bade859f160e7ab11144ddd91f3b5b7f
ugurhmz <craxx67@gmail.com>
2024-01-27 01:03:42 +0300

    Search Restaurant with desc added

--- back-end/controller/RestaurantController.js
+++ back-end/controller/RestaurantController.js
@@ -1,21 +1,13 @@
-import httpStatus from 'http-status'
-import RestaurantModel from '../models/RestaurantModel.js'
+import httpStatus from 'http-status';
+import RestaurantModel from '../models/RestaurantModel.js';
 
 // Create Restaurant Controller
 export const createRestaurantController = async (req, res) => {
   try {
-    const {
-      name,
-      description,
-      logo = 'restaurant_default.png',
-      address,
-      menus = [],
-      types = [],
-      branches = [],
-    } = req.body
+    const { name, description, logo = 'restaurant_default.png', address, menus = [], types = [], branches = [] } = req.body;
 
     if (!name || !description || !address) {
-      return res.status(httpStatus[400]).json({ error: 'Name, description, and address are required fields.' })
+      return res.status(httpStatus.BAD_REQUEST).json({ error: 'Name, description, and address are required fields.' });
     }
 
     const newRestaurant = new RestaurantModel({
@@ -26,14 +18,39 @@ export const createRestaurantController = async (req, res) => {
       menus,
       types,
       branches,
-    })
+    });
 
-    
-    const savedRestaurant = await newRestaurant.save()
+    const savedRestaurant = await newRestaurant.save();
 
-    res.status(httpStatus.CREATED).json(savedRestaurant)
+    res.status(httpStatus.CREATED).json(savedRestaurant);
   } catch (error) {
-    console.error('Error in createRestaurantController:', error.message)
-    res.status(httpStatus[500]).json({ error: 'Internal Server Error' })
+    console.error('Error in createRestaurantController:', error.message);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+  }
+};
+
+// ?searchQ=kebap&latitude=39.93&longitude=32.85
+
+// Find Restaurants by Search Query Controller
+export const findRestaurantsBySearchController = async (req, res) => {
+  try {
+    const { searchQ } = req.query
+
+    if (!searchQ) {
+      return res.status(httpStatus.BAD_REQUEST).json({ error: 'Search query is required.' })
+    }
+
+    const filter = { description: { $regex: new RegExp(searchQ, 'i') } }
+
+    const result = await RestaurantModel.find(filter)
+
+    if (result.length === 0) {
+      return res.status(httpStatus.OK).json({ message: 'No results found.' })
+    }
+
+    res.status(httpStatus.OK).json(result)
+  } catch (error) {
+    console.error('Error in findRestaurantsBySearchController:', error.message)
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' })
   }
 }
--- back-end/routes/restaurant.js
+++ back-end/routes/restaurant.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createRestaurantController } from '../controller/RestaurantController.js'
+import { createRestaurantController, findRestaurantsBySearchController} from '../controller/RestaurantController.js'
 import validate from '../middleware/validation.js'
 import {  menuValidation } from '../validations/RestaurantValidate.js'
 import { createMenuController } from '../controller/MenuController.js'
@@ -8,5 +8,6 @@ const router = express.Router()
 
 router.post('/create',createRestaurantController)
 router.post('/create-menu',validate(menuValidation) ,createMenuController)
+router.get('/get-restaurant', findRestaurantsBySearchController)
 
 export default router
